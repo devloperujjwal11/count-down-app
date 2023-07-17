95ff38ac7042e35a55050d702b380ca3ca4431f1
devloperujjwal11 <137766523+devloperujjwal11@users.noreply.github.com>
2023-07-17 22:37:16 +0530

    Add files via upload

---
+++ count down app/script.js
@@ -0,0 +1,26 @@
+const daysel = document.getElementById('days');
+const hoursel = document.getElementById('hours');
+const minsel = document.getElementById('mins');
+const secondsel = document.getElementById('seconds');
+const newyears = '1 jan 2024';
+
+function countdown(){
+    const newyearsdate = new Date(newyears);
+    const currentdate = new Date();
+const totalseconds = (newyearsdate - currentdate)/1000;
+const days = Math.floor(totalseconds/3600/24);
+const hours =Math.floor(totalseconds/3600)%24;
+const mins = Math.floor(totalseconds/60)%60;
+const seconds = Math.floor(totalseconds)%60;
+
+  daysel.innerHTML = days;
+  hoursel.innerHTML = hours;
+  minsel.innerHTML = mins;
+  secondsel.innerHTML = seconds;
+
+ 
+}
+
+countdown();
+
+setInterval(countdown,1000);
